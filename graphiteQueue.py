566c69e49a3cd0ea5d3716111d30e0f6cfb79a66
Ricky Moorhouse <MOORH@uk.ibm.com>
2022-11-20 16:34:46 +0000

    move to grafana

--- graphiteQueue.py
+++ graphiteQueue.py
@@ -1,24 +1,40 @@
-import socket
 import time
 import os
 import logging
+import requests
+
+
 class graphite():
     server = 'localhost'
     prefix = ""
-    port = 2003
     cache = []
 
     def __init__(self, server='localhost', port=2003, prefix=''):
         self.server = os.getenv('GRAPHITE_HOST', 'localhost')
-        self.port = int(os.getenv('GRAPHITE_PORT', '2003'))
+        self.url = 'https://{}/graphite/metrics'.format(self.server)
+        self.headers = {
+            "Authorization": "Bearer {}:{}".format(
+                os.getenv('GRAFANA_USER'),
+                os.getenv('GRAFANA_APIKEY'))
+        }
         self.prefix = prefix
         self.logger = logging.getLogger(__name__)
 
     def stage(self, name, value, timestamp=None):
-        if None == timestamp:
+        if timestamp is None:
             timestamp = int(time.time())
         message = '{}.{} {} {}'.format(self.prefix, name, value, timestamp)
-        self.cache.append(message)
+        self.cache.append(
+            {
+                'name': "{}.{}".format(self.prefix, name),
+                'metric': "{}.{}".format(self.prefix, name),
+                'value': float(value),
+                'time': int(timestamp),
+                'interval': 10,
+                'mtype': 'count',
+                'tags': [],
+            }
+        )
         self.logger.debug("storing {}".format(message))
 
     def debug(self):
@@ -26,19 +42,21 @@ class graphite():
 
     def store(self):
         try:
-            sock = socket.socket()
-            sock.connect((self.server, self.port))
-            self.logger.info("Sending {} records to graphite [{}:{}]".format(len(self.cache), self.server, self.port))
-            self.logger.debug("\n".join(self.cache))
-            sock.sendall(("\n".join(self.cache)+"\n").encode())
-            sock.close()
+            # sort by ts
+            self.cache.sort(key=lambda obj: obj['time'])
+            result = requests.post(self.url, json=self.cache, headers=self.headers)
+            if result.status_code != 200:
+                raise Exception(result.text)
+            print('%s: %s' % (result.status_code, result.text))            
             sent = len(self.cache)
             # Clear out cache
             self.cache[:] = []
             return sent
-        except socket.error as se:
+        except Exception as se:
             self.logger.exception(se)
 
     def __del__(self):
         logging.info("Storing remaining data ({} records)".format(len(self.cache)))
         self.store()
+
+
